8c05acddcad25cda55972a98504f4c37b847b12b
Honda Hiromichi <>
2018-10-04 02:35:16 +0900

    deviseの実装　新規登録時のエラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_151224) do
+ActiveRecord::Schema.define(version: 2018_10_03_171944) do
 
   create_table "features", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -58,15 +58,6 @@ ActiveRecord::Schema.define(version: 2018_10_01_151224) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "account_name", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "mail_address", null: false
-    t.string "password", null: false
-    t.integer "phone"
-    t.integer "role", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -79,6 +70,8 @@ ActiveRecord::Schema.define(version: 2018_10_01_151224) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
