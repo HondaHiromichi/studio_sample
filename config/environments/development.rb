429e13905f649c9143c026504c30af6da264b4ee
Honda Hiromichi <>
2018-10-05 00:24:37 +0900

    devise メール認証の実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,12 @@ Rails.application.configure do
     host: 'localhost',
     port: 3000
   }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: 'hiro940414@gmail.com',
+    password: 'hh51530414'
+  }
 end
