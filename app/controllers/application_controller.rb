a4f8fd17d4fb1fad0162ebae79e385a0a552635b
Honda Hiromichi <>
2018-10-13 23:05:03 +0900

    店舗運営者登録画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    added_attrs = [:account_name, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [:account_name, :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :phone, :role]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@
 #
 
 class User < ApplicationRecord
-  enum role: { user: 0, admin: 1}
+  enum role: { user: 0, admin: 1, manager: 2}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
