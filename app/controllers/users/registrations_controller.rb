682c499c75beff22d0d2a67cabd26ba01a72ce2e
Honda Hiromichi <>
2018-10-13 23:42:42 +0900

    プロフィール編集をcurrent passwordなしで更新可能に変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,4 +20,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :role])
   end
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
 end
